a151e44ee2abfbd3c1bb8072053b131c6214eeaf
Alena <alena.zhukova@skykick.com>
2020-01-29 21:30:11 +0100

    Added dices model

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Kitty!'
+        message: 'Hello Kitty!',
+        dices: [
+            { name: 'd4', value : 4, selected: false },
+            { name: 'd6', value : 6, selected: false },
+            { name: 'd8', value : 8, selected: false },
+            { name: 'd10', value : 10, selected: false },
+            { name: 'd12', value : 12, selected: false },
+            { name: 'd20', value : 20, selected: false },
+            { name: 'd100', value : 100, selected: false },
+        ],
     }
 });
